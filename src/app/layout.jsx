0f776e971d115df427d4e114a776ea2f7f02f263
DOF1109 <friasdaniel.1109@gmail.com>
2024-11-02 17:16:46 -0300

    Font changed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,17 +2,25 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../mui/theme";
 import { CssBaseline } from "@mui/material";
+import { Montserrat_Alternates } from "next/font/google";
 import "./globals.css";
 
 export const metadata = {
   title: "Delicias Andy",
   description: "Repostería Delicias Andy | De Andrea Ochoa",
+  keywords: "Delicias Andy, Reposteria, online",
 };
+
+const montserratAlternates = Montserrat_Alternates({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>
+      <body className={montserratAlternates.className}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
