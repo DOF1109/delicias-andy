90387fa3f452ee26cd31e65b52651c369fb4c25e
DOF1109 <friasdaniel.1109@gmail.com>
2024-11-07 14:15:31 -0300

    Header modified

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,43 @@
+"use client";
+const { AppBar, Container, Toolbar, Typography, Button, Box, alpha } = require("@mui/material");
+import { theme } from "@/mui/theme.js";
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar 
+      position="fixed" 
+      sx={{
+        boxShadow:0,
+        bgcolor: "transparent",
+        backgroundImage: "none",
+        mt: "1rem",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Toolbar 
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexShrink: "0",
+            borderRadius: "16px",
+            backdropFilter: "blur(24px)",
+            border: "1px solid",
+            borderColor: alpha("#d7d7e8", 0.5)
+          }}
+        >
+          <Box>
+            <Typography>Delicias Andy</Typography>
+            <Typography>Inicio</Typography>
+            <Typography>Servicios</Typography>
+            <Typography>Contacto</Typography>
+          </Box>
+          <Box>
+            <Button variant="contained" color="secondary">Haz tu pedido</Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
 
