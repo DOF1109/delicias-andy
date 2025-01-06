1adaf98091d22bc0fcaa1603d627c11a97a2b90e
DOF1109 <friasdaniel.1109@gmail.com>
2025-01-06 14:12:47 -0300

    Button's global styles modified on theme. Banner modiffied

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -1,5 +1,5 @@
 "use client";
-const { Box, Typography, Button } = require("@mui/material");
+const { Box, Typography, Button, Grid2 } = require("@mui/material");
 
 const Banner = () => {
   return (
@@ -14,16 +14,30 @@ const Banner = () => {
       }}
     >
       <Box>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h4" fontWeight={500}>
           Bienvenido a Delicias Andy
         </Typography>
-        <Typography>
+        <Typography variant="body2" mt={2} mb={5}>
           Descubre nuestras tortas y repostería, hechas con amor y los mejores
           ingredientes.
         </Typography>
         <Button variant="contained" color="secondary">
           Conócenos
         </Button>
+        <Grid2 container spacing={2} mt={5} mb={5}>
+          <Grid2>
+            <Typography>100%</Typography>
+            <Typography>Satisfacción garantizada</Typography>
+          </Grid2>
+          <Grid2>
+            <Typography>+50</Typography>
+            <Typography>Variedades de tortas</Typography>
+          </Grid2>
+          <Grid2>
+            <Typography>+10</Typography>
+            <Typography>Años de experiencia</Typography>
+          </Grid2>
+        </Grid2>
       </Box>
       <Box>Image</Box>
     </Box>
--- src/mui/theme.js
+++ src/mui/theme.js
@@ -18,6 +18,16 @@ const theme = createTheme({
     "hsla(220, 30%, 5%, 0.09) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.07) 0px 8px 16px -5px",
   dark: "#4d041c",
   light: "#fff1f3",
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "50px",
+          textTransform: "none",
+        }
+      }
+    }
+  }
 });
 
 export default theme;
