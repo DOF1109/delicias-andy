49434e0f4b54f7dfecf6d400a244e579ca87ec09
DOF1109 <friasdaniel.1109@gmail.com>
2025-01-29 21:13:49 -0300

    ServiceBanner's backgrond image fixed

--- src/components/layout/ServicesBanner.jsx
+++ src/components/layout/ServicesBanner.jsx
@@ -1,79 +1,73 @@
-"use client"
+"use client";
 
 import { Box, Grid2, Typography, alpha, useTheme } from "@mui/material";
 import Image from "next/image";
-import cake1 from "@/../public/assets/images/cake1.jpeg"
+import cake1 from "@/../public/assets/images/cake1.jpeg";
 
 const ServicesBanner = () => {
-    const theme = useTheme()
+  const theme = useTheme();
 
-    return (
-        <Box
-            component="section"
-            sx={{
-                background: alpha(theme.palette.secondary.dark, 0.75),
-                position: "relative"
-            }}
-        >
-            <Image
-                src={cake1}
-                alt="Torta de fondo"
-                style={{
-                    width: "100%",
-                    height: "auto",
-                    objectFit: "cover",
-                    position: "absolute",
-                    zIndex: -1
-                }}
-            />
-            <Box 
-                color={theme.light}
-                display="flex"
-                flexDirection="column"
-                gap={{xs:6, sm:7, md:12}}
-                p={{xs:4, sm:6, md:7}}
-                py={{md: 12}}
-            >
-                <Typography>
-                    Explora nuestra variedad de tortas, masas dulces y repostería. Siente el sabor y la calidez en cada bocado.
-                </Typography>
-                <Grid2 container spacing={2}>
-                    <Grid2 size={{xs:12, sm:6, md:3}}>
-                        <Typography variant="h6" fontWeight={600}>
-                            +10
-                        </Typography>
-                        <Typography variant="body2">
-                            Años de experiencia
-                        </Typography>
-                    </Grid2>
-                    <Grid2 size={{xs:12, sm:6, md:3}}>
-                        <Typography variant="h6" fontWeight={600}>
-                            +50
-                        </Typography>
-                        <Typography variant="body2">
-                            Productos ofrecidos
-                        </Typography>
-                    </Grid2>
-                    <Grid2 size={{xs:12, sm:6, md:3}}>
-                        <Typography variant="h6" fontWeight={600}>
-                            +1.000
-                        </Typography>
-                        <Typography variant="body2">
-                            Clientes satisfechos
-                        </Typography>
-                    </Grid2>
-                    <Grid2 size={{xs:12, sm:6, md:3}}>
-                        <Typography variant="h6" fontWeight={600}>
-                            +500
-                        </Typography>
-                        <Typography variant="body2">
-                            Pedidos personalizados
-                        </Typography>
-                    </Grid2>
-                </Grid2>
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box
+      component="section"
+      sx={{
+        background: alpha(theme.palette.secondary.dark, 0.75),
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
+      <Image
+        src={cake1}
+        alt="Torta de fondo"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          position: "absolute",
+          zIndex: -1,
+        }}
+      />
+      <Box
+        color={theme.light}
+        display="flex"
+        flexDirection="column"
+        gap={{ xs: 6, sm: 7, md: 12 }}
+        p={{ xs: 4, sm: 6, md: 7 }}
+        py={{ md: 12 }}
+      >
+        <Typography>
+          Explora nuestra variedad de tortas, masas dulces y repostería. Siente
+          el sabor y la calidez en cada bocado.
+        </Typography>
+        <Grid2 container spacing={2}>
+          <Grid2 size={{ xs: 12, sm: 6, md: 3 }}>
+            <Typography variant="h6" fontWeight={600}>
+              +10
+            </Typography>
+            <Typography variant="body2">Años de experiencia</Typography>
+          </Grid2>
+          <Grid2 size={{ xs: 12, sm: 6, md: 3 }}>
+            <Typography variant="h6" fontWeight={600}>
+              +50
+            </Typography>
+            <Typography variant="body2">Productos ofrecidos</Typography>
+          </Grid2>
+          <Grid2 size={{ xs: 12, sm: 6, md: 3 }}>
+            <Typography variant="h6" fontWeight={600}>
+              +1.000
+            </Typography>
+            <Typography variant="body2">Clientes satisfechos</Typography>
+          </Grid2>
+          <Grid2 size={{ xs: 12, sm: 6, md: 3 }}>
+            <Typography variant="h6" fontWeight={600}>
+              +500
+            </Typography>
+            <Typography variant="body2">Pedidos personalizados</Typography>
+          </Grid2>
+        </Grid2>
+      </Box>
+    </Box>
+  );
+};
 
 export default ServicesBanner;
