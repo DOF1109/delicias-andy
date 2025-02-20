e10323a9c4b2c800570ae1bfeaa5708a1d5439ed
DOF1109 <friasdaniel.1109@gmail.com>
2025-02-20 17:06:59 -0300

    ContactHello component updated with new layout and contact information

--- src/components/layout/ContactHello.jsx
+++ src/components/layout/ContactHello.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 const { Box, Grid2, Typography, useTheme } = require("@mui/material");
 
 const ContactHello = () => {
@@ -9,7 +11,8 @@ const ContactHello = () => {
     <Grid2
       container
       component="section"
-      padding={4}
+      px={4}
+      py={6}
       spacing={4}
       sx={{
         background: `linear-gradient( ${theme.palette.secondary.dark}, ${theme.palette.secondary.light})`,
@@ -17,12 +20,41 @@ const ContactHello = () => {
       }}
     >
       <Grid2 size={{ xs: 12, md: 6 }}>
-        <Typography variant="h5" fontWeight={600}>
-          !Hola desde Delicias Andy!
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 4,
+            justifyContent: "space-between",
+            height: "100%",
+          }}
+        >
+          <Typography variant="h5" fontWeight={600}>
+            !Hola desde Delicias Andy!
+          </Typography>
+          <Typography>
+            Lunes a Viernes de 08:00 a 19:00 hs.
+            <br />
+            Estamos listos para ayudarte con tus pedidos.
+          </Typography>
+        </Box>
       </Grid2>
       <Grid2 size={{ xs: 12, md: 6 }}>
-        <Typography variant="h5">!Hola desde Delicias Andy!</Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+          <Typography variant="h6">Envíanos un mensaje o llámanos</Typography>
+          <Link href="tel:3873556647" className="blank-link">
+            <Typography>3873556647</Typography>
+          </Link>
+          <Typography variant="h6" mt={4}>
+            Síguenos en nuestras redes
+          </Typography>
+          <Link href="#" className="blank-link">
+            <Typography>Facebook</Typography>
+          </Link>
+          <Link href="#" className="blank-link">
+            <Typography>Instagram</Typography>
+          </Link>
+        </Box>
       </Grid2>
     </Grid2>
   );
